d41bcace4f31059ffd7f5d14bac17b9b58216bef
Bohain Mathis <mathis.malock@gmail.com>
2026-04-02 10:33:09 +0200

    Correction fichier

--- backend/utils/mesure.py
+++ backend/utils/mesure.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 import numpy as np
 
-from image_blocking import image_to_patch_vectors, load_grayscale_matrix
+from Traitement_Image.py import image_to_patch_vectors, load_grayscale_matrix
 
 _MODES_VALIDES = frozenset(
     {
